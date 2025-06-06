ee63d85073657049b533d5d1ae7440cf736a4f3b
tjolk <18270178+tjolk@users.noreply.github.com>
2025-06-06 08:19:48 +0000

    replaced config

---
+++ config.php
@@ -0,0 +1,2 @@
+<?php
+define('BASE_PATH', '/var/www/vhosts/buutvrij.nl/getij.buutvrij.nl');
--- php/updateTideData.php
+++ php/updateTideData.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../config.php';
+require_once __DIR__ . '/../../config_getij.php';
 
 $file = BASE_PATH . '/data/tideData.json';
 $logFile = BASE_PATH . '/data/update.log';
